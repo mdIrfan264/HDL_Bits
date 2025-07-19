0c7b02cfee112e91c9020dedd11f4270e6231fe3
mdIrfan264 <mdirfan19a@gmail.com>
2025-07-19 14:39:09 +0530

    Create Four_wires.v

---
+++ Verilog_language/Basics/Four_wires.v
@@ -0,0 +1,11 @@
+module top_module( 
+    input a,b,c,
+    output w,x,y,z );
+
+  // assign the output variables with the input as given 
+    assign w = a;
+    assign x = b;
+    assign y = b;
+    assign z = c;
+
+endmodule
