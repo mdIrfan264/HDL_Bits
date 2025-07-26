df9cf2d2539e9d0d07a336c71bb9fe79f855ee27
mdIrfan264 <mdirfan19a@gmail.com>
2025-07-26 10:22:08 +0530

    Create if_statement.v

---
+++ Verilog_language/procedure/if_statement.v
@@ -0,0 +1,17 @@
+
+module top_module(
+    input a,
+    input b,
+    input sel_b1,
+    input sel_b2,
+    output wire out_assign,
+    output reg out_always   ); 
+    
+    assign out_assign = (sel_b1 &sel_b2)?b:a;
+    
+    always@(*) begin
+         out_always = (sel_b1 &sel_b2)?b:a;
+        
+    end
+
+endmodule
