4b7d46a9596674633e6d8f23bea8b6f9e7ca1bc4
mdIrfan264 <mdirfan19a@gmail.com>
2025-07-28 13:17:11 +0530

    Create vector100r.v

---
+++ Verilog_language/More verilog features/vector100r.v
@@ -0,0 +1,12 @@
+module top_module( 
+    input [99:0] in,
+    output [99:0] out
+);
+    always@(*)begin
+        for (int i = 99 ; i>=0 ; i = i-1)
+            out[99-i] =  in[i]; 
+        
+        
+    end
+
+endmodule
